88bde8027bc3d0a7306775d69296e6902046126a
MohitTiwariBytes <mohittiwaridev@gmail.com>
2025-12-26 20:23:37 +0530

    text done + primary + secondary button done!

--- src/Components/Buttons/Animated/BtnAnimated.jsx
+++ src/Components/Buttons/Animated/BtnAnimated.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BtnAnimated.css";
-import gsap from "gsap";
 
 export default function BtnAnimated({ sx, text, onClick }) {
+  const renderChars = () =>
+    [...text].map((char, i) => (
+      <span
+        key={i}
+        className="btn-char"
+        style={{
+          transition: "all 0.4s cubic-bezier(.26,.2,0,1.31)",
+          whiteSpace: char === " " ? "pre" : "normal",
+        }}
+      >
+        {char}
+      </span>
+    ));
+
   return (
-    <div style={sx} onClick={onClick} className="btn-animated">
+    <div style={sx} onClick={onClick} className={`btn-animated`}>
       <div className="btn-left-icon">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M9.53033 2.21966L9 1.68933L7.93934 2.74999L8.46967 3.28032L12.4393 7.24999H1.75H1V8.74999H1.75H12.4393L8.46967 12.7197L7.93934 13.25L9 14.3107L9.53033 13.7803L14.6036 8.70709C14.9941 8.31657 14.9941 7.6834 14.6036 7.29288L9.53033 2.21966Z"
-            fill="#7D0303"
-          />
-        </svg>
+        <div className="wrap-svg-btn">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M9.53033 2.21966L9 1.68933L7.93934 2.74999L8.46967 3.28032L12.4393 7.24999H1.75H1V8.74999H1.75H12.4393L8.46967 12.7197L7.93934 13.25L9 14.3107L9.53033 13.7803L14.6036 8.70709C14.9941 8.31657 14.9941 7.6834 14.6036 7.29288L9.53033 2.21966Z"
+              fill="#7D0303"
+            />
+          </svg>
+        </div>
       </div>
+
       <div className="btn-text">
-        <span>{text}</span>
+        <div className="btn-text-inner">{renderChars()}</div>
       </div>
     </div>
   );
--- src/Pages/Home/Sections/Hero/Hero.jsx
+++ src/Pages/Home/Sections/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import gsap from "gsap";
 import Navbar from "../../../../Components/Navbar/Navbar";
+import BtnAnimated from "../../../../Components/Buttons/Animated/BtnAnimated";
 
 export default function Hero() {
   return (
@@ -12,12 +13,15 @@ export default function Hero() {
             Building sites for brands fed up with the generic{" "}
             <z id="strike">nonsense.</z>
           </h1>
-          <span>
+          <span id="desc-hero">
             My name is Mohit Tiwari and I am a 14 year old creative web dev from
             India, I help brands and people like you in making their website
             stand out from the rest of the competition. No bullsh#t, No
             shortcuts, Just real results.
           </span>
+          <div className="btn-hero">
+            <BtnAnimated text={"Get In Touch"}></BtnAnimated>
+          </div>
         </div>
       </div>
     </div>
