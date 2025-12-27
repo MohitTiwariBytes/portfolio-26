260da42a57a13ddd549de8c994f11b14fb53fad7
MohitTiwariBytes <mohittiwaridev@gmail.com>
2025-12-27 20:04:48 +0530

    Animated the first section!

--- src/Pages/Home/Sections/Hero/Hero.jsx
+++ src/Pages/Home/Sections/Hero/Hero.jsx
@@ -7,9 +7,14 @@ import regrese from "../../../../assets/Images/regreseProjectPhoto.webp";
 import quickcopies from "../../../../assets/Images/quickCopiesProjectPhoto.webp";
 import coding4good from "../../../../assets/Images/coding4GoodProjectPhoto.webp";
 import Marquee from "../../../../Components/MarqueeDirectional/Marquee";
+import SplitText from "gsap/SplitText";
+import CustomEase from "gsap/CustomEase";
+
+gsap.registerPlugin(SplitText, CustomEase);
 
 export default function Hero() {
   useEffect(() => {
+    CustomEase.create("backEase", ".26,.2,0,1.31");
     const setHeroMinHeight = () => {
       const hero = document.querySelector(".main-hero-section");
       if (!hero) return;
@@ -27,6 +32,77 @@ export default function Hero() {
     };
 
     setHeroMinHeight(); // run once on load
+
+    const split1 = new SplitText(".top-txt-hero h1", {
+      type: "words",
+      wordsClass: "word-top-h1",
+    });
+
+    gsap.fromTo(
+      ".word-top-h1",
+      {
+        transform: "scaleY(0.7)",
+        filter: "blur(2px)",
+        opacity: 0,
+      },
+      {
+        transform: "scaleY(1)",
+        filter: "blur(0px)",
+        opacity: 1,
+        ease: "backEase",
+        duration: 0.6,
+        stagger: 0.02,
+        delay: 0.3,
+      }
+    );
+    gsap.fromTo(
+      "#desc-hero",
+      {
+        opacity: 0,
+        filter: "blur(2px)",
+        y: "20px",
+      },
+      {
+        y: "0px",
+        filter: "blur(0px)",
+        opacity: 1,
+        duration: 0.4,
+        delay: 0.8,
+        ease: "backEase",
+      }
+    );
+    gsap.fromTo(
+      ".btn-hero",
+      {
+        opacity: 0,
+        filter: "blur(2px)",
+        y: "20px",
+      },
+      {
+        y: "0px",
+        filter: "blur(0px)",
+        opacity: 1,
+        duration: 0.4,
+        delay: 0.9,
+        ease: "backEase",
+      }
+    );
+    gsap.fromTo(
+      ".marquee-btm",
+      {
+        opacity: 0,
+        filter: "blur(2px)",
+        y: "20px",
+      },
+      {
+        y: "0px",
+        filter: "blur(0px)",
+        opacity: 1,
+        duration: 0.4,
+        delay: 0.9,
+        ease: "backEase",
+      }
+    );
   }, []);
 
   return (
